e71908e2ecf7da3f7f757b23cd5a68dc96cc1c1b
github username <github email address>
2020-01-15 22:13:11 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,49 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do
+    redirect to '/recipes'
+  end
+
+  # code actions here
+  get '/recipes/new' do #loads new form
+    erb :'/recipes/new'
+  end
+
+
+  get '/recipes' do #loads index page
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'/recipes/show'
+  end
+
+  get '/recipes/:id/edit' do #loads the edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'/recipes/edit'
+  end
+
+  patch '/recipes/:id' do #update a recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do #creates a recipe
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do #deletes a recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
 
 end
